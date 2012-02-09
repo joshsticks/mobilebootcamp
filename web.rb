6de71d589a944c12b6b8b0f8eaa90942d50a0ad7
Joshua Stickles <sticklesjb@gmail.com>
2012-02-09 14:27:18 -0500

    added coffeescript dependency and test, also added more folder structure

--- web.rb
+++ web.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'coffee-script'
 
 get '/' do
   haml :index
+end
+
+get '/public/javascripts/application.js' do
+  coffee :application
 end
